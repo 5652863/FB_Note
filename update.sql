c8458457ebd7b5b4bdfb14a85952f1875269cea0
LeGenD <5652863@gmail.com>
2022-07-07 12:11:06 +0300

    PKEVENT.ex_docgrp_stockgrpch_11

---
+++ update.sql
@@ -0,0 +1,6 @@
+insert into ENUMVALUE (IDENUM, IVALUE, SNAME, SCODEDEVELOP, BSIGNIFICANT, SHELP, SNAME_USR,
+                       SNAME_SHORT)
+values (127, 11,
+        'Выставляет событие контроль отгрузки',
+        'PKEVENT.ex_docgrp_stockgrpch_11', 1, null, null,
+        'Контроль отгрузки');
